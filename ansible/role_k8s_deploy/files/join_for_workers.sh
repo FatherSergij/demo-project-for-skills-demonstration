3683a0efa94f83d6bfa2ef61e989e5b1d580f265
FatherFedor <FatherFedor13@gmail.com>
2023-10-04 19:40:45 +0300

    22

--- ansible/role_k8s_deploy/files/join_for_workers.sh
+++ ansible/role_k8s_deploy/files/join_for_workers.sh
@@ -1,2 +1,2 @@
-kubeadm join 10.1.1.18:6443 --token anbcq6.amnz6hl22gkhq369 \
-	--discovery-token-ca-cert-hash sha256:64ce74f8f7652c06f2c57704280a516030dcf9cf3a1a45c32c3ba2d6369081cb 
+kubeadm join 10.1.1.45:6443 --token 66njgh.11nyg4cckynoo2ur \
+	--discovery-token-ca-cert-hash sha256:d3f18e541ab89dd0ca3b0b6165ca3884e9e6955d7bdfd1a21b8399c7ebabd8aa 
